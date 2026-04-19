ba4413f79f07e90c1526d032967bc30255115112
Rishi Raj <rishiraj88+github@gmail.com>
2026-04-19 21:38:10 +0200

    Add security_group and security_group_rule resources.

--- main.tf
+++ main.tf
@@ -14,10 +14,51 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"]
 }
 
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_instance" "portal" {
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.portal.id]
+
   tags = {
-    Name = "Hallo Frankfurt"
+    Name = "PoC Terraform"
   }
+}
+
+resource "aws_security_group" "portal" {
+  name = "portal"
+  tags = {
+    Terraform = "true"
+  }
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "portal_http_in" {
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.portal.id
+}
+
+resource "aws_security_group_rule" "portal_https_in" {
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.portal.id
+}
+
+resource "aws_security_group_rule" "portal_everything_out" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.portal.id
 }
