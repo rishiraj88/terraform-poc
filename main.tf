a057220f73948718a22598dfa03a21f25889975a
Rishi Raj <rishiraj88+github@gmail.com>
2026-05-06 06:26:03 +0200

    Use rules from Terraform registry and update security group.

--- main.tf
+++ main.tf
@@ -21,44 +21,21 @@ data "aws_vpc" "default" {
 resource "aws_instance" "portal" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.portal.id]
+  vpc_security_group_ids = [module.portal_sg.security_group_id] # security_group_id : Terraform output type
 
   tags = {
     Name = "PoC Terraform"
   }
 }
 
-resource "aws_security_group" "portal" {
-  name = "portal"
-  tags = {
-    Terraform = "true"
-  }
-  vpc_id = data.aws_vpc.default.id
-}
+module "portal_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.13.0"
 
-resource "aws_security_group_rule" "portal_http_in" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.portal.id
-}
-
-resource "aws_security_group_rule" "portal_https_in" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.portal.id
-}
-
-resource "aws_security_group_rule" "portal_everything_out" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.portal.id
+  vpc_id  = data.aws_vpc.default.id
+  name    = "portal"
+  ingress_rules = ["https-443-tcp","http-80-tcp"] # the rules from Terraform repo
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
