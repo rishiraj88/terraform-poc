4027b36bfa68ad2a5b43a495f692e798103b5f38
Rishi Raj <rishiraj88+github@gmail.com>
2026-05-06 06:56:58 +0200

    Use 'VPC' module from Terraform and declare our VPC for portal.

--- main.tf
+++ main.tf
@@ -14,8 +14,22 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"]
 }
 
-data "aws_vpc" "default" {
-  default = true
+module "portal_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a","eu-west-1b","eu-west-1c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+  
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
 
 resource "aws_instance" "portal" {
@@ -23,6 +37,7 @@ resource "aws_instance" "portal" {
   instance_type          = var.instance_type
   vpc_security_group_ids = [module.portal_sg.security_group_id] # security_group_id : Terraform output type
 
+  subnet_id = module.portal_vpc.public_subnets[0]
   tags = {
     Name = "PoC Terraform"
   }
@@ -32,7 +47,8 @@ module "portal_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
 
-  vpc_id  = data.aws_vpc.default.id
+  vpc_id  = module.portal_vpc.vpc_id
+  
   name    = "portal"
   ingress_rules = ["https-443-tcp","http-80-tcp"] # the rules from Terraform repo
   ingress_cidr_blocks = ["0.0.0.0/0"]
